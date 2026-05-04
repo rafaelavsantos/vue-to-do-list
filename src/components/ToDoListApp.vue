c94d95ce9a5c57a231e2a44415dec4d2efc2b8a9
Rafaela Santos <rafaelavsantosdev@gmail.com>
2026-05-04 11:01:58 -0300

    feat: add task statistics (total, completed, pending)

--- src/components/ToDoListApp.vue
+++ src/components/ToDoListApp.vue
@@ -2,6 +2,9 @@
     <div class="container" style="max-width: 800px;">
         <h1 class="text-center my-4">To-do List</h1>
 
+        <!-- Stats -->
+        <TaskStats :tasks="tasks" />
+
         <!-- Add new task -->
         <TaskAdd @add-task="addTask" />
 
@@ -11,18 +14,19 @@
 <script setup>
 import { useStorage } from '@vueuse/core';
 import TaskAdd from './partials/TaskAdd.vue';
+import TaskStats from './partials/TaskStats.vue';
 
 const tasks = useStorage('tasks', []);
 
 const addTask = (task) => {
-  if (!task) { return }
+    if (!task) { return }
 
-  tasks.value.push({
-    id: Date.now(),
-    name: task,
-    completed: false,
-    state: 'show' // edit, delete
-  })
+    tasks.value.push({
+        id: Date.now(),
+        name: task,
+        completed: false,
+        state: 'show' // edit, delete
+    })
 };
 
 
