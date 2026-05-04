d51ba4d0470ea2ee9f73f1fae868c65b9f784827
Rafaela Santos <rafaelavsantosdev@gmail.com>
2026-05-04 10:59:30 -0300

    feat: add task

--- src/components/ToDoListApp.vue
+++ src/components/ToDoListApp.vue
@@ -1,9 +1,29 @@
 <template>
     <div class="container" style="max-width: 800px;">
         <h1 class="text-center my-4">To-do List</h1>
+
+        <!-- Add new task -->
+        <TaskAdd @add-task="addTask" />
+
     </div>
 </template>
 
 <script setup>
+import { useStorage } from '@vueuse/core';
+import TaskAdd from './partials/TaskAdd.vue';
+
+const tasks = useStorage('tasks', []);
+
+const addTask = (task) => {
+  if (!task) { return }
+
+  tasks.value.push({
+    id: Date.now(),
+    name: task,
+    completed: false,
+    state: 'show' // edit, delete
+  })
+};
+
 
 </script>
---
+++ src/components/partials/TaskAdd.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="input-group mb-3">
+        <input v-model="task" type="text" placeholder="Adicione uma nova tarefa" class="form-control"
+            @keyup.enter="addTask">
+        <button @click="addTask" class="btn btn-primary">Adicionar</button>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const task = ref('');
+
+const emit = defineEmits(['add-task']);
+
+const addTask = () => {
+  if(!task.value) return;
+
+  emit('add-task', task.value);
+  
+  task.value = '';
+}
+
+</script>
